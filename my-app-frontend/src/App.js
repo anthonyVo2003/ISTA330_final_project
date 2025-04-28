613b1824e363983addbc83b0d3fd5bb8606f5f44
Anthony Vo <anthonyvo@arizona.edu>
2025-04-28 07:17:35 -0700

    separated login and registration

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Register from './components/Register';
 import Login from './components/Login';
 
 const App = () => {
+  const [showRegister, setShowRegister] = useState(true);
+
   return (
     <div>
-      <Register />
-      <Login />
+      <nav>
+        <button onClick={() => setShowRegister(true)}>Register</button>
+        <button onClick={() => setShowRegister(false)}>Login</button>
+      </nav>
+
+      {showRegister ? <Register /> : <Login />}
     </div>
   );
 };
 
 export default App;
+
